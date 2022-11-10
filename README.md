e85f6a4856ff0185fb15d5b2d056edd3cfcfe9ce
Sean <hustle0985@gmail.com>
2022-11-10 14:25:39 +0800

    1110 update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # f2e_2022_sean
-This is my side project for the f2e 2022 contest.
+
+網站連結：
 https://lashawty.github.io/f2e_2022_sean/
+
+專案說明：
+*f2e 2022 week1
+*html(pug)分為導覽列、主選單、主要內容、頁尾
+*css(sass)除上方結構各自CSS檔案，另有mixin, reset, variable(變數及共用元件), font
+*JS 使用jQuery、原生JS，分為動畫專用 gsap.js /其他元件 app.js
--- gsap.js
+++ gsap.js
@@ -1,24 +1,48 @@
 //GSAP
 gsap.registerPlugin(ScrollTrigger, TextPlugin)
-const tl = gsap.timeline({
+
+//宣告
+const tl1 = gsap.timeline({
   scrollTrigger: {
     trigger: ".view", // 決定scrolltrigger要以哪一個元素作為觸發基準點
     markers: true, // 開啟start & end標記點，單純方便瀏覽動畫開始與結束點
-    start: 'top 640px', // 決定動畫開始點的位置
-    end: 'top 540px', // 決定動畫結束點的位置
+    start: 'top 600px', // 決定動畫開始點的位置
+    end: 'top 0', // 決定動畫結束點的位置
     scrub: true, //重要！開啟scrub來決定動畫播放是否依賴視窗滾動
   },
 })
 
-tl.to('.dialog-1', {
-  // top: '50px', // 距外層容器top 0px
-  left: '0', // 距外層容器left 50%
-  // xPercent: '0', // translate(-50%, 0)
-  duration: 10, // 動畫持續時間之比例
-  // position: 'absolute', // position: 'absolute'，絕對位置才能使用left, right...等
+//物件動畫設定
+tl1.to('.ghost', {
+  right: '0',
+  duration: 10,
+}).to('.chi', {
+  left: '0',
+  duration: 10,
+}).to('.dialog-1', {
+  left: '0',
+  duration: 10,
 }).to('.dialog-2', {
   right: '0',
-  // yPercent: '-100', // translate(0, -100%)
   duration: 10,
-  // position: 'absolute',
+}).to('.option-block-1', {
+  bottom: '0',
+  duration: 10,
+}).to('.dialog-1', {
+  left: '-1000px',
+  duration: 10, 
+}).to('.ghost', {
+  right: '-2000px',
+  duration: 10,
+}).to('.contest-text', {
+  left: '300px',
+  duration: 10,
+}).to('.option-block-1', {
+  bottom: '-500px',
+  duration: 10,
+}).to('.option-block-2', {
+  bottom: '0',
+  duration: 10,
 })
+
+
